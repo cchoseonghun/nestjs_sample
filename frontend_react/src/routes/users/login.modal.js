d19e824e30ba228cac7f0fe502aee46d97578507
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-02-23 22:44:17 +0900

    feat: frontend_react - 이메일 인증 시스템 연결 완료

--- frontend_react/src/routes/users/login.modal.js
+++ frontend_react/src/routes/users/login.modal.js
@@ -1,9 +1,15 @@
+import axios from 'axios';
 import { useDispatch, useSelector } from "react-redux";
 import { setShow } from '../../store/modal.slice';
 import { Button, Modal, Form, InputGroup } from 'react-bootstrap';
+import { useState } from "react";
 
 
 function LoginModal() {
+  const [isVerified, setIsVerified] = useState(false);
+  const [email, setEmail] = useState('');
+  const [verifyToken, setVerifyToken] = useState('');
+
   let state = useSelector((state)=> state );
   let dispatch = useDispatch();
 
@@ -18,11 +24,11 @@ function LoginModal() {
         <Form>
           <Form.Group className="mb-3">
               <InputGroup className="mb-2">
-                <Form.Control id="email" name="email" type="email" placeholder='이메일' autoFocus />
+                <Form.Control id="email" name="email" type="email" placeholder='이메일' autoFocus onChange={(e) => { setEmail(e.target.value); setIsVerified(false); }}/>
                 <Button variant="success" onClick={()=>{ sendEmail(); }}>인증번호 전송</Button>
               </InputGroup>
               <InputGroup className="mb-2">
-                <Form.Control id="confirm_email" name="confirm_email" type="text" placeholder='인증번호' autoFocus />
+                <Form.Control id="confirm_email" name="confirm_email" type="text" placeholder='인증번호' autoFocus onChange={(e) => { setVerifyToken(e.target.value); }}/>
                 <Button variant="outline-success" onClick={()=>{ confirmEmail(); }}>인증번호 확인</Button>
               </InputGroup>
               <Form.Control id="password" className='mb-2' name="password" type="password" placeholder='비밀번호' autoFocus />
@@ -31,20 +37,53 @@ function LoginModal() {
         </Form>
       </Modal.Body>
       <div className="d-grid gap-2 m-2">
-        <Button variant="primary" onClick={()=>{ handleClose(); register(); }}>회원가입</Button>
+        <Button variant="primary" onClick={()=>{ register(); }}>회원가입</Button>
       </div>
     </Modal>
   )
 
   function confirmEmail() {
-    alert('인정번호 확인 준비중');
+    axios
+      .put(`http://localhost:8080/users/verifyEmail?verifyToken=${verifyToken}`, { email })
+      .then((response) => {
+        const statusCode = response.status;
+        // console.log('status code: ' + statusCode);
+        if (statusCode === 200) {
+          alert('인증완료');
+          setIsVerified(true);
+        }
+      })
+      .catch((e) => {
+        console.log('axios 통신실패');
+        alert(e.response.data.message);
+      });
   }
 
   function sendEmail() {
-    alert('인증번호 전송 준비중');
+    if (email.length == 0) {
+      alert('이메일을 입력해야 합니다.');
+      return;
+    }
+    axios
+      .post('http://localhost:8080/users/verifyEmail', { email })
+      .then((response) => {
+        const statusCode = response.status;
+        console.log('status code: ' + statusCode);
+        if (statusCode === 201) {
+          alert('인증번호를 이메일로 보냈습니다.');
+        }
+      })
+      .catch((e) => {
+        console.log('axios 통신실패');
+        console.log(e);
+      });
   }
 
   function register() {
+    if (!isVerified) {
+      alert('이메일 인증이 필요합니다.');
+      return;
+    }
     alert('준비중');
   }
 }
